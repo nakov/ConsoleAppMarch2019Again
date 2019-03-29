7bca1f557360fb8b8a2581e3e93d71322494feb3
Svetlin Nakov <github@nakov.com>
2019-03-29 21:25:24 +0200

    Add project files.

---
+++ ConsoleAppMarch2019Again/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppMarch2019Again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
