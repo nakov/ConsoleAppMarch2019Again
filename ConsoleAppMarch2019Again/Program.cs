5107cc4401825c49057425730419f44ee4962b1a
Svetlin Nakov <github@nakov.com>
2019-03-29 21:26:12 +0200

    Added my first file

--- ConsoleAppMarch2019Again/Program.cs
+++ ConsoleAppMarch2019Again/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppMarch2019Again
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
         }
     }
 }
